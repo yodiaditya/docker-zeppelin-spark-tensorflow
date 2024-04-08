5d5b887945e1c8533aecd852f1920d5662d6a5c8
yodiaditya <anaknewbie@gmail.com>
2024-04-09 04:05:43 +0700

    bug file fixed

--- zeppelin/conf/zeppelin-env.sh
+++ zeppelin/conf/zeppelin-env.sh
@@ -2,5 +2,4 @@ export ZEPPELIN_SPARK_CONCURRENTSQL=true
 export ZEPPELIN_INTP_MEM="-Xms1024m -Xmx24G -XX:MaxPermSize=1024m"
 export ZEPPELIN_MEM="-Xms1024m -Xmx24G -XX:MaxPermSize=1024m"
  
-export MASTER="spark://spark-master:7077"
 export ZEPPELIN_JAVA_OPTS="-Dspark.driver.memory=5g -Dspark.executor.memory50g -Dspark.cores.max=16"
