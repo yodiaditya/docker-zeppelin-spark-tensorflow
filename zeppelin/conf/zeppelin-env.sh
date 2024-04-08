fbd1b85980c57484c5cd870f49d3f1b8ae3cfb21
yodiaditya <anaknewbie@gmail.com>
2024-04-09 03:27:48 +0700

    Increase memory at zeppelin env files

--- zeppelin/conf/zeppelin-env.sh
+++ zeppelin/conf/zeppelin-env.sh
@@ -1,4 +1,6 @@
-export ZEPPELIN_MEM="-Xms1024m -Xmx2048m"
-export ZEPPELIN_INTP_MEM="-Xms1024m -Xmx2048m"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64"
-export CUDA_HOME=/usr/local/cuda
+export ZEPPELIN_SPARK_CONCURRENTSQL=true
+export ZEPPELIN_INTP_MEM="-Xms1024m -Xmx24G -XX:MaxPermSize=1024m"
+export ZEPPELIN_MEM="-Xms1024m -Xmx24G -XX:MaxPermSize=1024m"
+ 
+export MASTER="spark://spark-master:7077"
+export ZEPPELIN_JAVA_OPTS="-Dspark.driver.memory=5g -Dspark.executor.memory50g -Dspark.cores.max=16"
