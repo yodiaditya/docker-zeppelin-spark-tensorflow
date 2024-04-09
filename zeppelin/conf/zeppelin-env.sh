415925fca8391cf3955ec2ae56e2debd9da5c321
yodiaditya <anaknewbie@gmail.com>
2024-04-09 14:21:25 +0700

    use all memories and cores

--- zeppelin/conf/zeppelin-env.sh
+++ zeppelin/conf/zeppelin-env.sh
@@ -2,5 +2,5 @@ export ZEPPELIN_SPARK_CONCURRENTSQL=true
 export ZEPPELIN_INTP_MEM="-Xms1024m -Xmx24G -XX:MaxPermSize=1024m"
 export ZEPPELIN_MEM="-Xms1024m -Xmx24G -XX:MaxPermSize=1024m"
  
-export ZEPPELIN_JAVA_OPTS="-Dspark.driver.memory=5g -Dspark.executor.memory50g -Dspark.cores.max=16"
+export ZEPPELIN_JAVA_OPTS="-Dspark.driver.memory=5g -Dspark.executor.memory60g -Dspark.cores.max=16"
 export ZEPPELIN_INTERPRETER_DEP_MVNREPO="https://repo1.maven.org/maven2"
